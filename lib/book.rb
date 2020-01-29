60bf3cd17b02ee2342e8a36df750ce8ba78092ae
alexandre jamarco <alexandre.j@gmail.com>
2020-01-29 16:10:22 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+require "pry"
+class Book
+    attr_reader :title 
+    attr_accessor :author, :page_count, :genre
+    def initialize(name)
+        @title = name
+        @page_count = 0
+    end
 
+    def turn_page
+        self.page_count += 1
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
+#aaa = Book.new("test")
+#binding.pry
